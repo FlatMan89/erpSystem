3b1c70f6d89554f261c750a992cfb5ed770bd608
flatman <flatman@flatman-PC>
2016-01-13 00:14:30 +0800

    不知道搞了些啥东西

--- src/main/java/com/zcyp/erpSystem/App.java
+++ src/main/java/com/zcyp/erpSystem/App.java
@@ -2,11 +2,8 @@ package com.zcyp.erpSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Configuration//配置控制
 @EnableAutoConfiguration//启用自动配置
