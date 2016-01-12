bae53a38c0c5aa73212ddcbc915b1ec02de95209
FlatMan89 <yehunn@qq.com>
2016-01-12 17:46:04 +0800

    创建项目

---
+++ src/main/java/com/zcyp/erpSystem/App.java
@@ -0,0 +1,21 @@
+package com.zcyp.erpSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App 
+{
+	@RequestMapping("/")
+	public String hello(){
+		return "as大手大脚凯撒鹿鼎记萨拉";
+	}
+	
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
