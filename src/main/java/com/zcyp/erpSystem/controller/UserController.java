87e1a1a8eebd03efe385d766bc9427fbe48d198c
FlatMan89 <yehunn@qq.com>
2016-01-13 18:09:35 +0800

    修改了点东西，模板并没有测试成功

--- src/main/java/com/zcyp/erpSystem/controller/UserController.java
+++ src/main/java/com/zcyp/erpSystem/controller/UserController.java
@@ -1,34 +1,45 @@
 package com.zcyp.erpSystem.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.zcyp.erpSystem.dao.UserDao;
-import com.zcyp.erpSystem.entity.User;
+import com.zcyp.erpSystem.model.User;
+import com.zcyp.erpSystem.service.UserService;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 	@Autowired
-	UserDao userdao;
+	UserService userservice;
 	
 	@RequestMapping("/{id}")
 	public User view(@PathVariable("id") Long id){
-		User user = new User();
-		user.setId(id);
-		user.setName("jin");
-		return user;
+		return userservice.getUser(id);
 	}
 	
 	@RequestMapping("/findByName/{name}")
 	public User getUserByName(@PathVariable("name") String name){
-		return userdao.findByName(name);
+		return userservice.getUser(name);
 	}
 	
 	@RequestMapping("/getId/{name}")
 	public Long getUserIdByName(@PathVariable("name") String name){
-		return userdao.findByName(name).getId();
+		return userservice.getUser(name).getId();
+	}
+	
+	@RequestMapping("/list")
+	public List<User> getAll(){
+		return userservice.getAll();
+	}
+	
+	@RequestMapping("/save")
+	public User saveUser(User user){
+		return userservice.saveUser(user);
 	}
 }
--- src/main/java/com/zcyp/erpSystem/dao/UserDao.java
+++ src/main/java/com/zcyp/erpSystem/dao/UserDao.java
@@ -1,11 +1,33 @@
 package com.zcyp.erpSystem.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.zcyp.erpSystem.entity.User;
+import com.zcyp.erpSystem.model.User;
 
 @Transactional
 public interface UserDao extends CrudRepository<User, Long> {
+	/**
+	 * 根据姓名查找
+	 * @param name
+	 * @return
+	 */
 	public User findByName(String name);
+	
+	/**
+	 * 根据 ID 查找
+	 * @param id
+	 * @return
+	 */
+	public User findById(Long id);
+	
+	/**
+	 * 查找 全部
+	 * @return 
+	 */
+	public List<User> findAll();
+	
+	
 }
